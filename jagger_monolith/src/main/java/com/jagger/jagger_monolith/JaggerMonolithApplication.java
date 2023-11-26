0847f179c1e85592d5e77933e42120c5b06384e4
James Barry <jamesbarry@jamess-mbp-5.lan>
2023-11-25 20:11:42 -0600

    Adding the spring-boot and maven file structure

---
+++ jagger_monolith/src/main/java/com/jagger/jagger_monolith/JaggerMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.jagger.jagger_monolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaggerMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaggerMonolithApplication.class, args);
+	}
+
+}
---
+++ jagger_monolith/src/test/java/com/jagger/jagger_monolith/JaggerMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jagger.jagger_monolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaggerMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
